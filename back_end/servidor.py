71368bd666821e300c3b26aecbfc726af090d07a
Carlos Arjona Martin <carloscopernic@gmail.com>
2019-07-24 11:09:23 +0200

    +cambios

--- back_end/servidor.py
+++ back_end/servidor.py
@@ -8,7 +8,7 @@ from logica.Review import Review
 from logica.User import User
 from logica.Recomendation import Recomendation
 
-app = Flask(__name__, template_folder='../front_end', static_folder='../front_end/css') #nuevo objeto
+app = Flask(__name__, template_folder='../front_end', static_folder='../front_end') #nuevo objeto
 
 # Set the secret key to some random bytes. Keep this really secret!
 app.secret_key = 'the_wheel_of_time'
@@ -104,7 +104,7 @@ def review():
         experiences = []
         for experience in adminExperience.getAll():
             experiences.append(experience.toJSON())
-        return render_template('review.html', experiences = experiences)
+        return render_template('review.html', username=username, experiences = experiences)
 
 @app.route("/recomendate", methods=["GET"])
 def recomendate():
